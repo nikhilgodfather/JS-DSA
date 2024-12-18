6fe752d31eda95591e9ecc4df0007614ddfc8fe2
nikhil <nikhilgodfather>
2024-12-18 22:13:25 +0530

    array search question

---
+++ Search.js
@@ -0,0 +1,9 @@
+var search = function(nums, target) {
+    
+    if(nums.includes(target)){
+        return nums.indexOf(target)
+    }
+    return -1
+};
+
+console.log(search(nums = [1], target = 1))
